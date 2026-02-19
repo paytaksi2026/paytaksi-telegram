84c21b8e36a51b14b777e7c1ad6c2be952824ce2
paytaksi2026 <paytaksi2026@gmail.com>
2026-02-19 21:53:06 +0400

    Delete src directory

--- src/migrate.js
+++
@@ -1,6 +0,0 @@
-const fs=require('fs'); const path=require('path'); const {q}=require('./db');
-async function runMigrations(){
-  const sql=fs.readFileSync(path.join(__dirname,'..','sql','schema.sql'),'utf-8');
-  await q(sql);
-}
-module.exports={runMigrations};
