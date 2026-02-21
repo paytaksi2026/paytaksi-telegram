a7960d15b55b68d4f771ea62c743278dabb2a2bc
paytaksi2026 <paytaksi2026@gmail.com>
2026-02-21 09:20:07 +0400

    Delete backend directory

--- backend/src/db.js
+++
@@ -1,9 +0,0 @@
-import Database from 'better-sqlite3';
-
-const dbFile = process.env.DB_FILE || './data/paytaksi.sqlite';
-
-export function getDb() {
-  const db = new Database(dbFile);
-  db.pragma('journal_mode = WAL');
-  return db;
-}
